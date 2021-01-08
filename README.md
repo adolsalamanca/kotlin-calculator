027d7620ac4730a73cc79bee512fb4a1a0124762
Adolfo Rodriguez <adolsalamanca@gmail.com>
2021-01-08 22:14:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+## Kotlin Calculator
+
+<p>
+Small application that performs a few operations just to try Kotlin 
+</p>
+<p>
+
+## About Kotlin
+
+<p>
+It uses JVM and all his power, offers interoperability with Java, and it is also more concise, safer and funnier.
+</p>
+
+## Requirements
+<p>
+This POC assumes that the user has gradle installed.
+</p>
+
+* Gradle 6.7
+* OpenJDK 15
+
+## Running tests
+
+To run tests just
+```
+gradle test -i
+```
+
+Run the application
+```
+gradle run
+```
+
+## Author
+
+* **Adolfo Rodriguez** - *kotlin-calculator* - [adolsalamanca](https://github.com/adolsalamanca)
+
+
+## References
+
+* [Kotlin](https://kotlinlang.org/)
+
+
+## License
+
+This project is licensed under MIT License.
+
+
+
+
---
+++ app/src/test/kotlin/calculator/CalculatorTest.kt
@@ -0,0 +1,55 @@
+package calculator
+
+import java.lang.Float.POSITIVE_INFINITY
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CalculatorTest {
+    @Test fun should_sum_values() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(sum,1F,1F)
+
+        assertEquals(2F, result)
+    }
+
+    @Test fun should_subtract_values() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(subtract,5F,2F)
+
+        assertEquals(3F, result)
+    }
+
+    @Test fun should_multiply_values() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(multiply,2F,2F)
+
+        assertEquals(4F, result)
+    }
+
+    @Test fun should_divide_values() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(divide,10F,2F)
+
+        assertEquals(5F, result)
+    }
+
+    @Test fun should_calculate_percentage() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(percentage,10F,200F)
+
+        assertEquals(20F, result)
+    }
+
+    @Test fun should_crash_divide_by_zero() {
+        val calculator = Calculator()
+
+        val result = calculator.operation(divide,10F,0F)
+
+        assertEquals(POSITIVE_INFINITY, result)
+    }
+}
